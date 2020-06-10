a49ec823e81dfc584d5fcf0514c897d4f2faa6fe
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-10 02:41:01 -0300

    Refatorando a classe Server aproveitando o que o Gabriel B. tinha escrito

--- src/br/com/Server/Server.java
+++ src/br/com/Server/Server.java
@@ -1,83 +1,77 @@
 package br.com.Server;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Server extends Thread {
-	private static ServerSocket socket;
-	private static ArrayList<PrintWriter> clients;
+public class Server {
+	
+	private ServerSocket socket;
+	public ArrayList<ClientInfo> clients;
 	public static final int PORT = 5001;
-	private Socket conn;
-	private OutputStream ou;
-	private PrintWriter out;
-	private InputStream in;
-	private InputStreamReader inr;
-	private BufferedReader bfr;
 	
-	public Server(Socket conn){
-	   this.conn = conn;
-	   try {
-		   this.in  = conn.getInputStream();
-		   this.inr = new InputStreamReader(in);
-		   this.bfr = new BufferedReader(inr);
-		   this.ou = conn.getOutputStream();
-		   this.out = new PrintWriter(ou, true);
-		   
-		   clients.add(out);
-	   } catch (Exception e) {
-		   e.printStackTrace();
-	   }                          
-	}
-	
-	public static void main(String[] args) throws Exception {
-		socket = new ServerSocket(PORT);
-		clients = new ArrayList<>();
-		System.out.println("Server running (V0.4)");
-		while(true) {
-			Socket conn = socket.accept();
-			Thread thr = new Server(conn);
-			System.out.println("Connetion established: " + conn);
-			System.out.println(clients.size() + " client(s) connected");
-			thr.start();
-		}
+	// Construtor da classe Server
+	public Server() {
 		
-	}
-	
-	public void run() {
-		String msg = "something";
+		// try-catch para abrir o socket do servidor
 		try {
-			while(!msg.isEmpty()) {
-				if(bfr.ready()) {
-					msg = bfr.readLine();
-					System.out.println(conn.getPort()+":"+ msg);
-					sendToAll(msg);
-				}
-			}
-			clients.remove(out);
-			System.out.println("Connection ended:" + conn);
-			conn.close();
-		} catch(Exception e){
-			e.printStackTrace();
+			this.socket = new ServerSocket(PORT);
+		} catch (IOException e) {
+			System.out.println("Erro ao abrir socket do servidor: " + e.getMessage());
+			System.exit(1); // Encerra a execução do programa
 		}
 		
+		this.clients = new ArrayList<>();
 	}
 	
-	public void sendToAll(String msg) throws Exception {
-		for(PrintWriter pw: clients) {
-			if(!(pw == out)) {
-				System.out.print(".");
-				pw.println(msg);
-			}
+	// Método que replica a mensagem para todos os clientes conectados
+	// src_client é o cliente que enviou a mensagem, pode ser null, caso seja uma mensagem do servidor
+	public void broadcast(ClientInfo src_client, String msg) {
+		for(ClientInfo c: clients)
+			if(c != src_client)
+				c.writer.println(msg);
+	}
+	
+	// Envia uma mensagem ao cliente solicitando o seu nome
+	public void readClientName(ClientInfo client) {
+		client.writer.print("Digite seu nome: ");
+		client.writer.flush();
+		try {
+			client.name = client.reader.readLine();
 		}
-		System.out.println();
+		catch(Exception e) {
+			client.writer.println("Desculpe, ocorreu uma falha ao ler seu nome. Nomeado como \"Anonymous\"!");
+			client.name = "Anonymous";
+		}
 	}
 	
+	public static void main(String[] args) throws IOException {
+		Server s = new Server();
+		
+		System.out.println("Server iniciado e esperando por conexões!");
+		
+		while(true) {
+			Socket conn;
+			
+			// tenta estabelecer conexão com um cliente
+			try {
+				conn = s.socket.accept(); // aceita a conexão
+			} catch (IOException e) {
+				System.out.println("Não foi possível estabelecer conexão com cliente: " + e.getMessage());
+				continue;
+			}
+			
+			// log de entrada
+			System.out.println("Cliente conectado: " + conn.getRemoteSocketAddress());
+			
+			// cria o cliente apartir da conexão
+			ClientInfo client = new ClientInfo(conn, s);
+			
+			// cria uma thread de HandleClient que ficará responsável por gerenciar a conexão com o cliente
+			Thread thr = new HandleClient(client, s);
+			thr.start(); // inicia a thread para lidar com a conexão
+		}
 
-	
+	}
 }
